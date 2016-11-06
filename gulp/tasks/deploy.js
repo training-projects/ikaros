b7d4d3c31c839c6e3bc4661263cfee5215d979eb
theandersonn <theandersonn@gmail.com>
2016-11-06 12:16:49 -0200

    Update task deploy

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -6,6 +6,6 @@ var gulp = require('gulp');
 var deploy = require("gulp-gh-pages");
 
 gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
+  return gulp.src("../dist/**/*")
     .pipe(deploy())
 });
