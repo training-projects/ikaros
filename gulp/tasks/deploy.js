c31d3765f2c44bc49616303ce80c26e88a637b54
theandersonn <theandersonn@gmail.com>
2016-11-06 11:01:36 -0200

    Added task deploy

---
+++ gulp/tasks/deploy.js
@@ -0,0 +1,11 @@
+/*--------------------------------------------------------------
+    TASK DEPLOY
+--------------------------------------------------------------*/
+// plugins
+var gulp = require('gulp');
+var deploy = require("gulp-gh-pages");
+
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
